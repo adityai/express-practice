89d74ca68dd10319c29219cb51f16f8005e201dd
adityaii <adityaii@gmail.com>
2019-06-26 23:52:12 +0000

    Serve data from mongodb

--- app.js
+++ app.js
@@ -1,13 +1,36 @@
 const express = require('express');
-const app = express();
+const mongoose = require('mongoose');
 
+const app = express();
+const db = mongoose.connect('mongodb://localhost/bookAPI');
 const port = process.env.PORT || 3000;
 const bookRouter = express.Router();
+const Book = require('./models/bookModel');
 
 bookRouter.route('/books')
     .get((req, res) => {
-      const response = { "hello": "This is my express api practice for books web service"}
-      res.json(response);
+      const query = {};
+      if (req.query.genre) {
+        query.genre = req.query.genre;
+      }
+
+      Book.find(query, (err, books) => {
+        if (err) {
+          return res.send(err);
+        }
+        return res.json(books);
+      });
+});
+
+
+bookRouter.route('/books/:bookId')
+.get((req, res) => {
+  Book.findById(req.params.bookId, (err, book) => {
+    if (err) {
+      return res.send(err);
+    }
+    return res.json(book);
+  });
 });
 
 app.use('/api', bookRouter);
